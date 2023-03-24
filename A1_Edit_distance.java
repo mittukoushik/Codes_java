286a000861cf7323913c6cbc15e0784f44428bf3
mittukoushik <88316002+mittukoushik@users.noreply.github.com>
2023-03-24 13:21:25 +0530

    Create A1_Edit_distance.java

---
+++ A1_Edit_distance.java
@@ -0,0 +1,60 @@
+/*Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+Example 1:
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+
+Example 2:
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+ 
+Constraints:
+
+0 <= word1.length, word2.length <= 500
+word1 and word2 consist of lowercase English letters.
+
+*/
+import java.util.*;
+class j{
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String t=sc.next();
+        int n=s.length(),m=t.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        System.out.println(dp[n][m]);
+        
+    }
+}
